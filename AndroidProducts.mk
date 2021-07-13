fefd13f67ff3b7c6e8145258481e4b020013256c
Francis023 <francisluisclado@gmail.com>
2021-07-13 10:14:35 +0000

    Initial SSOS Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/ssos_juice.mk
+
+COMMON_LUNCH_CHOICES := \
+    ssos_juice-user \
+    ssos_juice-userdebug \
+    ssos_juice-eng
